3023dd7f3f544c02c918a286844a1c3a157e7ee6
akshay0121 <akshaycs123456@gmail.com>
2022-10-14 14:50:40 +0530

    descriptifg

--- lib/Domain/Staff/ToGuardian_TextSMS.dart
+++ lib/Domain/Staff/ToGuardian_TextSMS.dart
@@ -1,47 +1,39 @@
-class TextSMS_ToGuardian_initialValues {
+class TextSMSToGuardian {
+  List<TextSMSToGuardianCourseList>? courseList;
   bool? isClassTeacher;
-  bool? isDualAttendance;
-  List<TextSMS_ToGuardian_course>? course;
-
-  TextSMS_ToGuardian_initialValues(
-      {this.isClassTeacher, this.isDualAttendance, this.course});
-
-  TextSMS_ToGuardian_initialValues.fromJson(Map<String, dynamic> json) {
-    isClassTeacher = json['isClassTeacher'];
-    isDualAttendance = json['isDualAttendance'];
-    if (json['course'] != null) {
-      course = [];
-      json['course'].forEach((v) {
-        course!.add(TextSMS_ToGuardian_course.fromJson(v));
+  TextSMSToGuardian({this.courseList, this.isClassTeacher});
+  TextSMSToGuardian.fromJson(Map<String, dynamic> json) {
+    if (json['courseList'] != null) {
+      courseList = [];
+      json['courseList'].forEach((v) {
+        courseList!.add(TextSMSToGuardianCourseList.fromJson(v));
       });
     }
+    isClassTeacher = json['isClassTeacher'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = Map<String, dynamic>();
-    data['isClassTeacher'] = this.isClassTeacher;
-    data['isDualAttendance'] = this.isDualAttendance;
-    if (this.course != null) {
-      data['course'] = this.course!.map((v) => v.toJson()).toList();
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.courseList != null) {
+      data['courseList'] = this.courseList!.map((v) => v.toJson()).toList();
     }
 
+    data['isClassTeacher'] = this.isClassTeacher;
     return data;
   }
 }
 
-//course
-
-class TextSMS_ToGuardian_course {
+class TextSMSToGuardianCourseList {
   String? value;
   String? text;
   Null selected;
   Null active;
   int? order;
 
-  TextSMS_ToGuardian_course(
+  TextSMSToGuardianCourseList(
       {this.value, this.text, this.selected, this.active, this.order});
 
-  TextSMS_ToGuardian_course.fromJson(Map<String, dynamic> json) {
+  TextSMSToGuardianCourseList.fromJson(Map<String, dynamic> json) {
     value = json['value'];
     text = json['text'];
     selected = json['selected'];
@@ -60,19 +52,17 @@ class TextSMS_ToGuardian_course {
   }
 }
 
-//Division
-
-class TextSMS_ToGuardian_Division {
+class TextSMSToGuardianDivisionList {
   String? value;
   String? text;
   Null selected;
   Null active;
   Null order;
 
-  TextSMS_ToGuardian_Division(
+  TextSMSToGuardianDivisionList(
       {this.value, this.text, this.selected, this.active, this.order});
 
-  TextSMS_ToGuardian_Division.fromJson(Map<String, dynamic> json) {
+  TextSMSToGuardianDivisionList.fromJson(Map<String, dynamic> json) {
     value = json['value'];
     text = json['text'];
     selected = json['selected'];
@@ -92,7 +82,10 @@ class TextSMS_ToGuardian_Division {
 }
 
 //View
-class TExtSMS_VIEW_byStaff {
+class TextSMSToGuardianCourseDivision_notification_Stf {
+  String? studentId;
+  String? guardianId;
+
   String? admnNo;
   String? name;
   int? rollNo;
@@ -101,8 +94,10 @@ class TExtSMS_VIEW_byStaff {
   String? course;
   bool? selected;
 
-  TExtSMS_VIEW_byStaff(
-      {this.admnNo,
+  TextSMSToGuardianCourseDivision_notification_Stf(
+      {this.studentId,
+      this.guardianId,
+      this.admnNo,
       this.name,
       this.rollNo,
       this.mobNo,
@@ -110,7 +105,10 @@ class TExtSMS_VIEW_byStaff {
       this.course,
       this.selected});
 
-  TExtSMS_VIEW_byStaff.fromJson(Map<String, dynamic> json) {
+  TextSMSToGuardianCourseDivision_notification_Stf.fromJson(
+      Map<String, dynamic> json) {
+    studentId = json['studentId'];
+    guardianId = json['guardianId'];
     admnNo = json['admnNo'];
     name = json['name'];
     rollNo = json['rollNo'];
@@ -121,12 +119,95 @@ class TExtSMS_VIEW_byStaff {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['studentId'] = this.studentId;
+    data['guardianId'] = this.guardianId;
     data['admnNo'] = this.admnNo;
     data['name'] = this.name;
     data['rollNo'] = this.rollNo;
     data['mobNo'] = this.mobNo;
     data['division'] = this.division;
     data['course'] = this.course;
+    return data;
+  }
+}
+
+//SMS Format
+
+class SmsSettingsByStaff {
+  List<SmsFormatByStaff>? smsFormat;
+  String? applySMSFormatApproval;
+
+  SmsSettingsByStaff({this.smsFormat, this.applySMSFormatApproval});
+
+  SmsSettingsByStaff.fromJson(Map<String, dynamic> json) {
+    if (json['smsFormat'] != null) {
+      smsFormat = [];
+      json['smsFormat'].forEach((v) {
+        smsFormat!.add(SmsFormatByStaff.fromJson(v));
+      });
+    }
+    applySMSFormatApproval = json['applySMSFormatApproval'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.smsFormat != null) {
+      data['smsFormat'] = this.smsFormat!.map((v) => v.toJson()).toList();
+    }
+    data['applySMSFormatApproval'] = this.applySMSFormatApproval;
+    return data;
+  }
+}
+
+class SmsFormatByStaff {
+  String? text;
+  String? value;
+  bool? isApproved;
+
+  SmsFormatByStaff({this.text, this.value, this.isApproved});
+
+  SmsFormatByStaff.fromJson(Map<String, dynamic> json) {
+    text = json['text'];
+    value = json['value'];
+    isApproved = json['isApproved'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['text'] = this.text;
+    data['value'] = this.value;
+    data['isApproved'] = this.isApproved;
+    return data;
+  }
+}
+
+//sms formats complete view
+
+class SmsFormatsCompleteview {
+  String? id;
+  String? name;
+  String? smsBody;
+  Null? content;
+  bool? isApproved;
+
+  SmsFormatsCompleteview(
+      {this.id, this.name, this.smsBody, this.content, this.isApproved});
+
+  SmsFormatsCompleteview.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    smsBody = json['smsBody'];
+    content = json['content'];
+    isApproved = json['isApproved'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['smsBody'] = this.smsBody;
+    data['content'] = this.content;
+    data['isApproved'] = this.isApproved;
     return data;
   }
 }
